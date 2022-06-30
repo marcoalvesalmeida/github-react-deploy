6f36a5e538f84da897729d9e25af3793a0303749
Marco Almeida <marco.almeida@netshow.me>
2022-06-30 20:48:30 -0300

    feat: add netlify in app

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Deploy React
+          Deploy React Netlify
         </a>
       </header>
     </div>
